0ad33088b0b0fd973e0cc24506afa794d9c9ef05
Fabio <fabioramosnic@gmail.com>
2023-01-13 12:09:35 -0300

    Todo front funcionando

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fd-portfolio-1606c',
+    appId: '1:296810421531:web:516ba69df0b121fd6187be',
+    storageBucket: 'fd-portfolio-1606c.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBlr1L_d4HbeKT__mE1vgUvAGcdvmLMR48',
+    authDomain: 'fd-portfolio-1606c.firebaseapp.com',
+    messagingSenderId: '296810421531',
+  },
 
   production: true,
   URL: 'http://localhost:8080/'
